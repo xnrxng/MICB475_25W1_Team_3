da9f8a7289fa0100a6d18193d7d844461937366c
xnrxng <xinruixiang@hotmail.com>
2025-10-11 20:03:22 -0700

    data processed

--- bin/01-qiime2_data_processing.sh
+++ bin/01-qiime2_data_processing.sh
@@ -3,22 +3,27 @@
 # This script processed the FASTQ files using QIIME2
 # Date: October 10th 2025
 
+### exit on errors
+set -euo pipefail
+
 ### activate conda env
 ### this script should be run in the server as it takes some absolute paths
-conda activate qiime2-2025.4
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate qiime2-amplicon-2025.4
 
 ### create a dir for where the data will be stored
-mkdir -r data/data_raw
-mkdir -r data/data_processed
+mkdir -p data/data_raw
+mkdir -p data/data_processed
 cd data
 
-# import data and demultiplex
+### import data and demultiplex
 qiime tools import \
   --type "SampleData[SequencesWithQuality]" \
   --input-format SingleEndFastqManifestPhred33V2 \
   --input-path /datasets/project_2/colombia/colombia_manifest.tsv \
   --output-path data_raw/demux_seqs.qza
 
+### convert into qzv file
 qiime demux summarize \
---i-data demux_seqs.qza \
+--i-data data_raw/demux_seqs.qza \
 --o-visualization data_raw/demux.qzv
