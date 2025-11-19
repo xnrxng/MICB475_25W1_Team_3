2af7878b33ea43ec5e9e14f714dd697a993d2e78
xnrxng <xinruixiang@hotmail.com>
2025-11-19 02:37:35 -0800

    used correct data in aim2

--- R/04-aim1_alpha_diversity.R
+++ R/04-aim1_alpha_diversity.R
@@ -15,186 +15,153 @@ library(phytools)
 library(dplyr)
 library(ggpubr)
 library(viridis)
+library(RColorBrewer)
+library(ggsignif)
 set.seed(2025)
 
+main <- function(){
 # Read data
-meta <- read.delim("data/data_raw/colombia_metadata.txt", sep = "\t")
-otu <- read.delim(file = "data/data_processed/feature-table.txt", sep = "\t", skip = 1, row.names = 1)
-taxa <- read.delim(file = "data/data_processed/taxonomy.tsv", sep = "\t")
-tree <- read.tree(file = "data/data_processed/tree.nwk")
-
-# Convert into df. Change rownames to be the sample names. Get rid of sample_name
-meta_df <- as.data.frame(meta[,-1])
-rownames(meta_df) <- meta$X.SampleID
-sample <- sample_data(meta_df)
-
-# Convert it into matrix 
-otu_mat <- as.matrix(otu)
-otu_tb <- otu_table(otu_mat, taxa_are_rows = TRUE)
-
-# Get rid of confidence column. Separate taxon into 7 columns. Convert into matrix
-taxa_mat <- taxa |> 
-  dplyr::select(-Confidence) |>
-  separate(col = Taxon, sep = "; ",
-           into = c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")) |>
-  as.matrix()
-
-# Get rid of feature id as a column and make it the rownames
-taxa_mat <- taxa_mat[,-1]
-rownames(taxa_mat) <- taxa$`Feature.ID`
-taxa_tb <- tax_table(taxa_mat)
-
-# Convert it into the phyloseq object
-phyloseq_obj <- phyloseq(otu_tb, sample, taxa_tb, tree)
-
-# Keep bacteria. Get rid of chloroplasts and mitochondria
-phyloseq_obj <- subset_taxa(phyloseq_obj, Domain == "d__Bacteria" & Class!="c__Chloroplast" & Family !="f__Mitochondria")
+  phyloseq_obj <- readRDS("data/data_processed/phyloseq_obj_rarefied.rds")
 
 # Extract required components
 OTU <- as.data.frame(otu_table(phyloseq_obj))
 TREE <- phy_tree(phyloseq_obj)
 
-# Resolve polytomies (make the tree dichotomous)
+# Resolve polytomies
 TREE_dich <- ape::multi2di(TREE)
-# Check if branch lengths exist
-head(TREE_dich$edge.length)
-# Force ultrametric
-ape::is.ultrametric(TREE_dich)
+
+# Apply Grafen branch lengths (ultrametric)
 TREE_ultra <- ape::compute.brlen(TREE_dich, method = "Grafen")
 
-# Ensure tree is rooted
-TREE_rooted <- ape::root(TREE_dich, outgroup = TREE_dich$tip.label[1], resolve.root = TRUE)
-# Make it fully bifurcating
-TREE_rooted_dich <- ape::multi2di(TREE_rooted)
-# Use phytools::chronos() clone = chronopl (so install + use phytools)
-TREE_ultra <- phytools::chronopl(TREE_rooted_dich, lambda = 1)
-
-# Ensure tree is dichotomous
-TREE_dich <- ape::multi2di(TREE_rooted_dich)
-# Apply Grafen branch lengths
-TREE_ultra <- ape::compute.brlen(TREE_dich, method = "Grafen")
-# Check
-ape::is.ultrametric(TREE_ultra)
-# Fix zero-length branches
+# Fix any zero-length edges
 TREE_ultra$edge.length[TREE_ultra$edge.length == 0] <- 1e-6
 
-# Compute node ages using branching.times()
+# Calculate node ages if running picante functions requiring ages
 node_ages <- ape::branching.times(TREE_ultra)
-# Attach ages to the tree for picante
 ntip  <- length(TREE_ultra$tip.label)
 nnode <- TREE_ultra$Nnode
+
 TREE_ultra$ages <- c(
-  rep(0, ntip),    # Tips have age 0
-  node_ages        # Internal nodes get branching times
+  rep(0, ntip),
+  node_ages
 )
 names(TREE_ultra$ages) <- 1:(ntip + nnode)
 
 # Fix the OTU table orientation
 OTU_pd <- as.data.frame(t(OTU))
 # Check that columns = taxa
-all(colnames(OTU_pd) == TREE_ultra$tip.label)
+# all(colnames(OTU_pd) == TREE_ultra$tip.label)
 # Calculate Faith’s PD (data frame (faith_pd) with Faith’s PD for each sample (rows correspond to samples, columns include PD and SR: species richness))
 faith_pd <- picante::pd(OTU_pd, TREE_ultra, include.root = TRUE)
 
 # Merge faith_pd with metadata
-meta$SampleID <- meta$X.SampleID
+meta <- sample_data(phyloseq_obj)
+meta$SampleID <- rownames(meta)
+meta <- data.frame(meta)
 faith_pd$SampleID <- rownames(faith_pd)
 meta_pd <- merge(meta, faith_pd, by = "SampleID")
-nrow(meta_pd)  # Should now match number of samples
+# nrow(meta_pd)  # Should now match number of samples
 
 # Create the lifestyle_group column
-meta_pd <- meta_pd %>%
+meta_pd <- meta_pd |>
   mutate(
     lifestyle_group = case_when(
-      fiber >= 20 & MET_mins_per_week >= 1000 ~ "adequate fibre;high exercise",
-      fiber >= 20 & MET_mins_per_week < 1000  ~ "adequate fibre;low exercise",
-      fiber < 20  & MET_mins_per_week >= 1000 ~ "inadequate fibre;high exercise",
+      fiber >= 20 & MET_mins_per_week > 1000 ~ "adequate fibre;high exercise",
+      fiber >= 20 & MET_mins_per_week <= 1000  ~ "adequate fibre;low exercise",
+      fiber < 20  & MET_mins_per_week > 1000 ~ "inadequate fibre;high exercise",
       TRUE ~ "inadequate fibre;low exercise"
     )
   )
 # Check if it worked
-table(meta_pd$lifestyle_group)
+# table(meta_pd$lifestyle_group)
 # Make sure CV_status exists
-table(meta_pd$Cardiometabolic_status)
+# table(meta_pd$Cardiometabolic_status)
 # Create the 8-group factor
 meta_pd$group <- paste(meta_pd$lifestyle_group, meta_pd$Cardiometabolic_status, sep = "_")
-table(meta_pd$group)  # Check counts for each of 8 groups
+# table(meta_pd$group)  # Check counts for each of 8 groups
 
 # Save data
-saveRDS(meta_pd, "results/aim2/alpha_diversity/00-meta_pd_faith_pd.rds")
 write_tsv(meta_pd, "results/aim2/alpha_diversity/01-meta_pd_faith_pd.tsv")
-saveRDS(faith_pd, "results/aim2/alpha_diversity/02-faith_pd.rds")
-write_tsv(faith_pd, "results/aim2/alpha_diversity/03-faith_pd.tsv")
+write_tsv(faith_pd, "results/aim2/alpha_diversity/02-faith_pd.tsv")
 
 # Kruskal-Wallis test
-kruskal.test(PD ~ group, data = meta_pd)
-pairwise.wilcox.test(meta_pd$PD, meta_pd$group, 
-                     p.adjust.method = "BH")  # Benjamini-Hochberg correction
-# Save data
-kw_res <- kruskal.test(PD ~ group, data = meta_pd)
-saveRDS(kw_res, "results/aim2/alpha_diversity/04-kruskal_wallis_group.rds")
-# Also save a tidy summary
-kw_summary <- data.frame(
-  statistic = kw_res$statistic,
-  df = kw_res$parameter,
-  p_value = kw_res$p.value
+healthy_pd <- meta_pd |>
+  filter(Cardiometabolic_status == "Healthy")
+kw_res_healthy <- kruskal.test(PD ~ group, data = healthy_pd)
+
+abnormal_pd <- meta_pd |>
+  filter(Cardiometabolic_status == "Abnormal")
+kw_res_abnormal <- kruskal.test(PD ~ group, data = abnormal_pd)
+
+# save a tidy summary
+kw_summary_healthy <- data.frame(
+  statistic = kw_res_healthy$statistic,
+  df = kw_res_healthy$parameter,
+  p_value = kw_res_healthy$p.value
 )
-write_tsv(kw_summary, "results/aim2/alpha_diversity/05-kruskal_wallis_group.tsv")
+
+kw_summary_abnormal <- data.frame(
+  statistic = kw_res_abnormal$statistic,
+  df = kw_res_abnormal$parameter,
+  p_value = kw_res_abnormal$p.value
+)
+
+kw_summary <- rbind(kw_summary_healthy, kw_summary_abnormal)
+rownames(kw_summary) <- c("healthy", "abnormal")
+
+write_tsv(kw_summary, "results/aim2/alpha_diversity/03-kruskal_wallis_group.tsv")
+
 # More saving
 pairwise_res <- pairwise.wilcox.test(meta_pd$PD, meta_pd$group, p.adjust.method = "BH")
-saveRDS(pairwise_res, "results/aim2/alpha_diversity/06-pairwise_wilcox_group.rds")
+saveRDS(pairwise_res, "results/aim2/alpha_diversity/04-pairwise_wilcox_group.rds")
 # Tidy version for easier reading
-pairwise_df <- as.data.frame(pairwise_res$p.value) %>%
+pairwise_df <- as.data.frame(pairwise_res$p.value) |>
   tibble::rownames_to_column("Group1")
-write_tsv(pairwise_df, "results/aim2/alpha_diversity/07-pairwise_wilcox_group.tsv")
+write_tsv(pairwise_df, "results/aim2/alpha_diversity/05-pairwise_wilcox_group.tsv")
 
-# Define a clean theme (for plots)
-theme_clean <- theme_bw(base_size = 14) +
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
-    axis.text.y = element_text(size = 12),
-    axis.title = element_text(size = 14, face = "bold"),
-    legend.title = element_text(size = 12, face = "bold"),
-    legend.text = element_text(size = 11),
-    plot.title = element_text(hjust = 0.5, size = 16, face = "bold")
-  )
+# boxplot
+kw_summary <- kw_summary |>
+  mutate(label = paste0("KW p = ", signif(p_value, 3)))
 
-# Plot Faith’s PD
-gg_final <- ggplot(meta_pd, aes(x = group, y = PD, fill = group)) +
-  geom_boxplot() +
-  theme_bw() +
-  labs(x = "Lifestyle × CV status", y = "Faith's PD") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("results/aim2/alpha_diversity/08-faith_PD_boxplot1.png", 
-       plot = gg_final, 
-       width = 10, height = 6, units = "in", dpi = 300)
+kw_summary$Cardiometabolic_status <- c("Healthy", "Abnormal")
 
-gg_final2 <- ggplot(meta_pd, aes(x = lifestyle_group, y = PD, fill = lifestyle_group)) +
+bp <- ggplot(meta_pd, aes(x = lifestyle_group, y = PD, fill = lifestyle_group)) +
   geom_boxplot(outlier.shape = 21, color = "black") +
   facet_wrap(~ Cardiometabolic_status, scales = "free_x") +
-  scale_fill_viridis_d(option = "C") +
   labs(
-    x = "Lifestyle group",
+    x = NULL,
     y = "Faith's PD",
     title = "Faith's PD by Lifestyle and CV Status"
   ) +
-  theme_bw(base_size = 14) +
+  theme_minimal() +
   theme(
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
-    axis.text.y = element_text(size = 12),
-    axis.title = element_text(size = 14, face = "bold"),
+    panel.border = element_rect(color = "black", fill = NA),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(colour = "black"),
+    panel.spacing = unit(0, "lines"),
     legend.position = "none",
-    plot.title = element_text(hjust = 0.5, size = 16, face = "bold")
-  )
-ggsave("results/aim2/alpha_diversity/09-faith_PD_boxplot2.png", 
-       plot = gg_final2, 
-       width = 10, height = 6, units = "in", dpi = 300)
+    plot.margin = margin(10, 10, 10, 10)) +
+  scale_fill_brewer(
+    palette = "Set2")+
+  scale_x_discrete(labels = c(
+    "adequate fibre;high exercise"    = "High fibre\nHigh exercise",
+    "adequate fibre;low exercise"     = "High fibre\nLow exercise",
+    "inadequate fibre;high exercise"  = "Low fibre\nHigh exercise",
+    "inadequate fibre;low exercise"   = "Low fibre\nLow exercise"
+  )) +
+  geom_text(
+    data = kw_summary,
+    aes(x = -Inf, y = Inf, label = label),
+    inherit.aes = FALSE,
+    hjust = -0.1,
+    vjust = 1.2,
+    size = 4
+  ) 
 
-# Boxplot with significance annotations
-gg_final3 <- ggboxplot(meta_pd, x = "group", y = "PD", 
-          color = "group", palette = "jco") +
-  rotate_x_text(angle = 45) +
-  stat_compare_means(method = "kruskal.test")
-ggsave("results/aim2/alpha_diversity/10-kruskal_test_boxplot1.png", 
-       plot = gg_final3, 
+ggsave("results/aim2/alpha_diversity/06-faith_PD_boxplot.png", 
+       plot = bp, 
        width = 10, height = 6, units = "in", dpi = 300)
+}
+
+main()
